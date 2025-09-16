e064cadfc860548042df2e6932f49a70280c5537
Juan Rodrigo Ferro Rayón <a01659298@tec.mx>
2025-09-16 15:51:12 -0600

    Agregué FrontEnd para aceptar y modificar reportes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,12 +5,19 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para permitir peticiones desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:5173', // Cambia esto si tu frontend está en otro puerto/origen
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
-  .setTitle("API de Gestión de Usuarios")
-  .setDescription("API para gestionar usuarios con autenticación JWT")
-  .setVersion("1.0")
-  .addBearerAuth() // Añadir esta línea para habilitar la autenticación Bearer en Swagger UI
-  .build();
+    .setTitle("API de Gestión de Usuarios")
+    .setDescription("API para gestionar usuarios con autenticación JWT")
+    .setVersion("1.0")
+    .addBearerAuth() // Añadir esta línea para habilitar la autenticación Bearer en Swagger UI
+    .build();
   const doc = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, doc);
   await app.listen(process.env.PORT ?? 3000);
