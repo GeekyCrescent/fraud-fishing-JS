b260618a102dba1e59c4c878b350f048e351e2fd
Vosquez <145882029+Vosquez@users.noreply.github.com>
2025-10-16 19:10:36 -0600

    Dashboard Axios

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -61,7 +61,7 @@ export default function Dashboard() {
   return (
     <div className="flex h-screen bg-gray-50">
       {/* Sidebar */}
-      <aside className="w-64 h-screen flex flex-col bg-white border-r border-teal-600"> {/* ← CAMBIO: border-r-1 → border-r */}
+      <aside className="w-64 h-screen flex flex-col bg-white border-r border-teal-600">
         {/* Logo */}
         <div className="p-6 text-2xl font-bold text-teal-700">
           Fraud<span className="text-black">Fishing</span>
